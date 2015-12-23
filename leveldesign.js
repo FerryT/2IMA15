326bdc44ab37fb3a4b0c185d89facb70b6559c54
Ferry Timmers <f.timmers@student.tue.nl>
2015-12-23 20:57:38 +0100

    used chaining in level design

--- leveldesign.js
+++ leveldesign.js
@@ -1,117 +1,114 @@
 function InitializeLevels()
 {
-	//game.addLevel(LevelZero());
 	game.addLevel(LevelOne());
+	game.addLevel(LevelZero());
 	game.addLevel(LevelTwo());
 	game.addLevel(LevelThree());
 	game.addLevel(LevelFour());
 }
 
+// Todo: concrete points instead of random ones
+
 function LevelZero()
 {	
-	var level = new Level('Runaround');
-
-	var groupOne = [new Entity(game.rect.randomPoint(), 0, new Behavior().Coward(1)),
-					new Entity(game.rect.randomPoint(), 0, new Behavior().Coward(1)),
-					new Entity(game.rect.randomPoint(), 0, new Behavior().Coward(1)),
-					new Entity(game.rect.randomPoint(), 0, new Behavior().Coward(1))];
-	var groupTwo = [new Entity(game.rect.randomPoint(), 1, new Behavior().Coward(5)),
-					new Entity(game.rect.randomPoint(), 1, new Behavior().Coward(5)),
-					new Entity(game.rect.randomPoint(), 1, new Behavior().Coward(5)),
-					new Entity(game.rect.randomPoint(), 1, new Behavior().Coward(5))];
-
-	level.add(groupOne);
-	level.add(groupTwo);
-	level.add(new Goal(new Line(0, .5, 1, .5).multiply(game.rect), 40));
-			
+	var behavior1 = new Behavior().Coward(1).Clamped(game.rect),
+		behavior2 = new Behavior().Coward(5).Clamped(game.rect),
+		level = new Level('Runaround')
+			.add(new Goal(new Line(0, .5, 1, .5).multiply(game.rect), 40))
+			// Group one
+			.add(new Entity(game.rect.randomPoint(), 0, behavior1))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior1))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior1))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior1))
+			//Group two
+			.add(new Entity(game.rect.randomPoint(), 1, behavior2))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior2))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior2))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior2))
+	;
 	return level;
 }
 
 function LevelOne()
 {	
-	var level = new Level('I\'ll wait', new Behavior().Editable()),
-		behavior = new Behavior().Draggable().Clamped(game.rect);
-
-	var groupOne = [new Entity(game.rect.randomPoint(), 0, behavior),
-					new Entity(game.rect.randomPoint(), 0, behavior),
-					new Entity(game.rect.randomPoint(), 0, behavior),
-					new Entity(game.rect.randomPoint(), 0, behavior)];
-	var groupTwo = [new Entity(game.rect.randomPoint(), 1, behavior),
-					new Entity(game.rect.randomPoint(), 1, behavior),
-					new Entity(game.rect.randomPoint(), 1, behavior),
-					new Entity(game.rect.randomPoint(), 1, behavior)];
-
-	level.add(groupOne);
-	level.add(groupTwo);
-	level.add(new Goal(new Line(0, .5, 1, .5).multiply(game.rect), 40));
-			
+	var behavior = new Behavior().Draggable().Clamped(game.rect),
+		behaviorL = new Behavior().Editable(behavior),
+		level = new Level('I\'ll wait', behaviorL)
+			.add(new Goal(new Line(0, .5, 1, .5).multiply(game.rect), 40))
+			// Group one
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			//Group two
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+		;
 	return level;
 }
 
 function LevelTwo()
 {	
-	var level = new Level('Jump!');
-
-	var groupOne = [new Entity(game.rect.randomPoint(), 0, new Behavior().Bounce(20)),
-					new Entity(game.rect.randomPoint(), 0, new Behavior().Bounce(20)),
-					new Entity(game.rect.randomPoint(), 0, new Behavior().Bounce(20)),
-					new Entity(game.rect.randomPoint(), 0, new Behavior().Bounce(20))];
-	var groupTwo = [new Entity(game.rect.randomPoint(), 1, new Behavior().Bounce(20)),
-					new Entity(game.rect.randomPoint(), 1, new Behavior().Bounce(20)),
-					new Entity(game.rect.randomPoint(), 1, new Behavior().Bounce(20)),
-					new Entity(game.rect.randomPoint(), 1, new Behavior().Bounce(20))];
-	
-	level.add(groupOne);
-	level.add(groupTwo);
-	level.add(new Goal(new Line(0, .5, 1, .5).multiply(game.rect), 40));
-			
+	var behavior = new Behavior().Bounce(20).Clamped(game.rect),
+		level = new Level('Jump!')
+			.add(new Goal(new Line(0, .5, 1, .5).multiply(game.rect), 40))
+			// Group one
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			//Group two
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+		;
 	return level;
 }
 
 function LevelThree()
 {	
-	var level = new Level('Runnin\' with the devil!');
-
-	var behavior1 = new Behavior().Pieter(1);
-	var groupOne = [new Entity(game.rect.randomPoint(), 0, Behavior.None),
-					new Entity(game.rect.randomPoint(), 0, Behavior.None),
-					new Entity(game.rect.randomPoint(), 0, Behavior.None),
-					new Entity(game.rect.randomPoint(), 0, Behavior.None)];
-	var groupTwo = [new Entity(game.rect.randomPoint(), 1, behavior1),
-					new Entity(game.rect.randomPoint(), 1, behavior1),
-					new Entity(game.rect.randomPoint(), 1, behavior1),
-					new Entity(game.rect.randomPoint(), 1, behavior1)];
-	
-	level.add(groupOne);
-	level.add(groupTwo);
-	level.add(new Goal(new Line(0, .5, 1, .5).multiply(game.rect), 40));
-			
+	var behavior1 = Behavior.None,
+		behavior2 = new Behavior().Pieter(1).Clamped(game.rect),
+		level = new Level('Runnin\' with the devil!')
+			.add(new Goal(new Line(0, .5, 1, .5).multiply(game.rect), 40))
+			// Group one
+			.add(new Entity(game.rect.randomPoint(), 0, behavior1))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior1))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior1))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior1))
+			// Group two
+			.add(new Entity(game.rect.randomPoint(), 1, behavior2))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior2))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior2))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior2))
+		;
 	return level;
 }
 
 function LevelFour()
 {	
-	var level = new Level('Somebody get me a doctor');
-
-	var behavior1 = new Behavior().Pieter(0.5);
-	var groupOne = [new Entity(game.rect.randomPoint(), 0, behavior1),
-					new Entity(game.rect.randomPoint(), 0, behavior1),
-					new Entity(game.rect.randomPoint(), 0, behavior1),
-					new Entity(game.rect.randomPoint(), 0, behavior1),
-					new Entity(game.rect.randomPoint(), 0, behavior1),
-					new Entity(game.rect.randomPoint(), 0, behavior1),
-					new Entity(game.rect.randomPoint(), 0, behavior1)];
-	var groupTwo = [new Entity(game.rect.randomPoint(), 1, behavior1),
-					new Entity(game.rect.randomPoint(), 1, behavior1),
-					new Entity(game.rect.randomPoint(), 1, behavior1),
-					new Entity(game.rect.randomPoint(), 1, behavior1),
-					new Entity(game.rect.randomPoint(), 1, behavior1),
-					new Entity(game.rect.randomPoint(), 1, behavior1),
-					new Entity(game.rect.randomPoint(), 1, behavior1)];
-	
-	level.add(groupOne);
-	level.add(groupTwo);
-	level.add(new Goal(new Line(0, .5, 1, .5).multiply(game.rect), 40));
-			
+	var behavior = new Behavior().Pieter(0.5).Clamped(game.rect),
+		level = new Level('Somebody get me a doctor')
+			.add(new Goal(new Line(0, .5, 1, .5).multiply(game.rect), 40))
+			// Group one
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			.add(new Entity(game.rect.randomPoint(), 0, behavior))
+			// Group two
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+			.add(new Entity(game.rect.randomPoint(), 1, behavior))
+		;
 	return level;
 }
